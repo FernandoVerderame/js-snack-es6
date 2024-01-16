1fcacebfefc450c40fc0e4ed072623ed834178b4
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-16 16:48:21 +0100

    added guests array

---
+++ snack-1/js/snack-1.js
@@ -0,0 +1,15 @@
+// Aggiungo l'array degli invitati
+const tableName = 'Tavolo Vip';
+
+const guests = [
+  'Brad Pitt',
+  'Johnny Depp',
+  'Lady Gaga',
+  'Cristiano Ronaldo',
+  'Georgina Rodriguez',
+  'Chiara Ferragni',
+  'Fedez',
+  'George Clooney',
+  'Amal Clooney',
+  'Maneskin'
+];
