47b392f81df2b8d290762fd6d97922b5c2880ecf
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-17 18:46:51 +0100

    added snack 4 solution

---
+++ snack-4/js/snack-4.js
@@ -0,0 +1,48 @@
+// Creo un array di squadre
+const teams = [
+    {
+        name: 'Salernitana',
+        points: 0,
+        fouls: 0
+    }, 
+    {
+        name: 'Napoli',
+        points: 0,
+        fouls: 0
+    }, 
+    {
+        name: 'Milan',
+        points: 0,
+        fouls: 0
+    }, 
+    {
+        name: 'Inter',
+        points: 0,
+        fouls: 0
+    }, 
+    {
+        name: 'Juventus',
+        points: 27,
+        fouls: 0
+    } 
+];
+
+// Creo un nuovo array con stesso nome ma con valori randomici in punti fatti e falli subiti
+const newTeams = teams.map(team => {
+
+    const randomPoints = Math.floor(Math.random() * 100) + 1;
+
+    const randomFouls = Math.floor(Math.random() * 50) + 1;
+
+    return {name: team.name, points: randomPoints, fouls: randomFouls} 
+});
+
+console.log(newTeams);
+
+
+// Creo un nuovo array composto dai nomi ed i falli subiti
+const nameFoulsStats = newTeams.map(newTeam => {
+    return {name: newTeam.name, fouls: newTeam.fouls}
+});
+
+console.log(nameFoulsStats);
